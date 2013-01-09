aa6bf4a576f2655573c5c758f52de1a684fb6396
marcelogmillani@gmail.com <marcelogmillani@gmail.com@43af39a8-7dbd-1c42-69be-1318f3ba3123>
2013-01-09 12:56:18 +0000

    Leitura de números binários, decimais e hexadecimais.

---
+++ branches/cpp/Montador/tests/numbers/readNum.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#include <string>
+
+#include "numbers.hpp"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	if(argc != 2)
+	{
+		printf("usage: readnum <number>\n");
+		return -1;
+	}
+
+	string *n = new string(argv[1]);
+
+	Number *num = new Number();
+	int value = num->toInt(n);
+
+	printf("%d\n",value);
+
+	return 0;
+
+}
